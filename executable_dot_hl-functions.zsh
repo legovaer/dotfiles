df1374af870709a5ff04eb897ad9209a65bff82f
levi govaerts <legovaer@me.com>
2024-01-07 13:07:12 +0100

    Add docker folder

--- executable_dot_hl-functions.zsh
+++ executable_dot_hl-functions.zsh
@@ -4,7 +4,7 @@ set +xe
 
 function hl-docker() {
   sudo apt-get update
-  sudo apt-get install ca-certificates curl gnupg
+  sudo apt-get install ca-certificates curl gnupg -y
   sudo install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -16,5 +16,7 @@ function hl-docker() {
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
 
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yn
+
+  mkdir /root/containers
 }
