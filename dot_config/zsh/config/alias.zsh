89ee02284b6636b4d759aee0cd9b69e7f5369b3f
Levi Govaerts <legovaer@me.com>
2024-01-07 13:28:18 +0100

    Added aliases

---
+++ dot_config/zsh/config/alias.zsh
@@ -0,0 +1,13 @@
+alias aliases='chezmoi ~/.config/zsh/config/aliases.zsh ; reload ; echo "Aliases reloaded"'
+
+alias cm='chezmoi'
+alias cm-commit='chezmoi git add -all && chezmoi git commit ; chezmoi git push'
+alias cm-add='chezmoi add'
+
+alias d='docker'
+alias dc='docker compose'
+alias dcu='docker compose up'
+alias dcl='docker compose logs -f'
+alias dcs='docker compose stop'
+alias dcp='docker compose pull'
+alias ld='lazydocker'
